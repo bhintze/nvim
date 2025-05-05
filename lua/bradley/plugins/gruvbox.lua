2263a867b08cdc5d0d02ced0d0615632c1752e37
Bradley Hintze <bradley.hintze@duke.edu>
2025-05-05 08:27:59 -0400

    Update gruvbox - remove highlighting

--- lua/bradley/plugins/gruvbox.lua
+++ lua/bradley/plugins/gruvbox.lua
@@ -3,5 +3,7 @@ return {
     priority = 1000,
     init = function()
       vim.cmd.colorscheme 'gruvbox'
+      vim.api.nvim_set_hl(0, 'String', { bg = 'none' })
+      vim.api.nvim_set_hl(0, 'Comment', { bg = 'none' })
     end,
 }
